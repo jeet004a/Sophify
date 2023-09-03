dd961f944a8f6cc6b4c450ab2b9c9c9a9550cd99
wastijeet110@gmail.com <wastijeet110@gmail.com>
2023-09-03 15:27:16 +0530

    Final Commit stage 1

--- shopify/settings.py
+++ shopify/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(*vzs^g(45^w6507ju334+=0o9mqv6iq2iulvvz*ixoehenmye
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
